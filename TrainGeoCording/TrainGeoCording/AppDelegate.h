79a091d9a6fbe1721734d48e9a8d362a4007e3f4
USER <user@USER-no-MacBook-Air.local>
2015-02-21 17:15:22 +0900

    create project

---
+++ TrainGeoCording/TrainGeoCording/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrainGeoCording
+//
+//  Created by USER on 2015/02/21.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrainGeoCording/TrainGeoCording/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrainGeoCording
+//
+//  Created by USER on 2015/02/21.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

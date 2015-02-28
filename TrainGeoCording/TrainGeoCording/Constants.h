fe8b811e4405680b989dc2670f3e7611c73ab5a5
USER <user@USER-no-MacBook-Air.local>
2015-02-28 18:12:16 +0900

    文字列とか定義ファイル追加

---
+++ TrainGeoCording/TrainGeoCording/Constants.h
@@ -0,0 +1,19 @@
+//
+//  Constants.h
+//  GetStationName
+//
+//  Created by USER on 2015/02/28.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+/// 駅名CSVファイル名
+#define CSV_FILE_NAME       @"station20141030free"
+
+/// 駅名CSVファイル拡張子
+#define CSV_FILE_EXTENSION  @"CSV"
+
+@end
---
+++ TrainGeoCording/TrainGeoCording/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  GetStationName
+//
+//  Created by USER on 2015/02/28.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
